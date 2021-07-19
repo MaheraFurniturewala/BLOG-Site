f5743895979fd2bd72ac851c29f6fb2346bdd04c
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-07-20 01:26:44 +0530

    set up the articles route

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const articleRouter = require('./routes/articles');
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+app.use('/articles',articleRouter);
+
+app.get('/',(req,res)=>{
+    //array of objects
+    const articles = [{
+        title : 'Test Articles',
+        createdAt : new Date(),
+        description  :'Test description'
+    }]
+    res.render('index',{ 
+        articles : articles
+    });
+});
+
+app.listen(8000);
