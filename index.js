d5164d8b52cd5c898e4c448b684bc54cea0afc91
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-07-20 17:02:19 +0530

    make the new article section

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require('express');
-const app = express();
+
 const articleRouter = require('./routes/articles');
+const mongoose = require('mongoose');
+const app = express();
+mongoose.connect('mongodb://localhost/blog_site',{ useNewUrlParser: true, useUnifiedTopology: true });
+const Article  = require('./models/article')
+
+
 app.set('view engine', 'ejs');
-app.set('views','./views');
 
-app.use('/articles',articleRouter);
+app.use(express.urlencoded({extended:false}));
 
-app.get('/',(req,res)=>{
+
+
+app.get('/', async (req,res)=>{
     //array of objects
-    const articles = [{
-        title : 'Test Articles',
-        createdAt : new Date(),
-        description  :'Test description'
-    }]
+    const articles = await Article.find().sort({
+        createdAt:'desc'
+    });
     res.render('articles/index',{ 
         articles : articles
     });
 });
+app.use('/articles',articleRouter);
+
 
 app.listen(8000);
--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,40 @@
 const express = require('express');
+const Article = require('./../models/article');
 const router = express.Router();
 
-
 router.get('/new',(req,res)=>{
-    res.render('articles/new');
+    //completely blank default article
+    res.render('articles/new',{article:new Article()});
 });
 
-router.post('/',(req,res)=>{
-
+router.get('/:id', async (req,res)=>{
+    const article = await Article.findById(req.params.id)
+    if(article == null){
+        res.redirect('/');
+    }
+    res.render('articles/show',{article : article})
 });
+
+router.post('/',async (req,res) => {
+    
+    let article = new Article({
+    title:req.body.title,
+    description:req.body.description,
+    markdown:req.body.markdown
+    })
+    try{
+        article = await article.save();
+        res.redirect(`/articles/${article.id}`)
+    }catch(e){
+        //it will take all the information yo addded in the article and shoow all of it again while going back to the same 
+        //but this will cause a problem if you are generally go from home page to new article because there you have not passed over the article
+        res.render('articles/new',{article: article})
+    }
+    
+})
+
+    
+    
+
 
 module.exports = router;
